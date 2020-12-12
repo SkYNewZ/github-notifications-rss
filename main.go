59b32727419e4148da09d60579beafd63bc5f064
Quentin Lemaire <quentin@lemairepro.fr>
2020-12-12 15:41:57 +0100

    feat: Enable HTTP access logs

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -17,21 +18,24 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/feed", getGithubNotificationsJSONFeed)
 	r.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "OK")
+		_, _ = fmt.Fprint(w, "OK")
 	})
+
+	// Log global router access logs
+	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
 
 	srv := &http.Server{
 		Addr:         addr + ":" + port,
 		WriteTimeout: time.Second * 15,
 		ReadTimeout:  time.Second * 15,
 		IdleTimeout:  time.Second * 60,
-		Handler:      r,
+		Handler:      loggedRouter,
 	}
 
 	go func() {
 		log.Printf("Listening on %s", srv.Addr)
 		if err := srv.ListenAndServe(); err != nil {
-			log.Println(err)
+			log.Fatalln(err)
 		}
 	}()
 
@@ -41,7 +45,7 @@ func main() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
 	defer cancel()
-	srv.Shutdown(ctx)
+	_ = srv.Shutdown(ctx)
 	log.Println("shutting down")
 	os.Exit(0)
 }
